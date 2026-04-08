60828c547065d45c38924a9461ad7ea6e93c4bbb
Phil Beber <phil@wabisabi.local>
2026-04-07 22:07:14 -0700

    Deploy to anamcarabodywork.com

--- sst.config.ts
+++ sst.config.ts
@@ -11,6 +11,14 @@ export default $config({
     };
   },
   async run() {
-    new sst.aws.Nextjs("MyWeb");
+    new sst.aws.Nextjs("MyWeb", {
+      domain:
+        $app.stage === "production"
+          ? {
+              name: "anamcarabodywork.com",
+              redirects: ["www.anamcarabodywork.com"],
+            }
+          : undefined,
+    });
   },
 });
